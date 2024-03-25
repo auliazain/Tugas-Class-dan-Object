7380cfcf48656c732de44ebbe27d153e61188932
zannahwahyunilubis45 <zannahwahyunilubis45@students.amikom.ac.id>
2024-03-25 18:03:47 +0700

    Add project files.

---
+++ Tugas Class dan Object/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas_Class_dan_Object
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Taxi taxi = new Taxi();
+
+            taxi.DriverName = "Jono";
+            taxi.OnDuty = true;
+            taxi.NumberPassenger = 10;
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOfPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tugas Class dan Object/taxi.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas_Class_dan_Object
+{
+    public class Taxi
+    {
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumberPassenger { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name : {0}", DriverName);
+            if (OnDuty == true)
+            {
+                Console.WriteLine("On Duty : Yes");
+            }
+            if (OnDuty == false)
+            {
+                Console.WriteLine("On Duty : No");
+            }
+            Console.WriteLine("Number Of Passenger : {0}", NumberPassenger);
+        }
+
+        public void DropOfPassenger()
+        {
+            Console.WriteLine("{0} selesai mengantar penumpang", DriverName);
+        }
+
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} sedang menjemput penumpang", DriverName);
+        }
+    }
+}
